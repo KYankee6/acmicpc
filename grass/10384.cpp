21e6ea050144ad93757caab83059491ad5675fea
KYankee6 <highestbright98@naver.com>
2022-07-03 16:51:47 +0900

    call it in

--- grass/10384.cpp
+++ grass/10384.cpp
@@ -5,12 +5,7 @@
 Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
-
-#include <iostream>
-#include <vector>
-#include <cctype>
-#include <map>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -25,14 +20,15 @@ int main()
     m[3] = "Triple pangram!!!";
     int T;
     cin>>T;
+    cin.ignore();
     for(int q=1; q<=T; q++){
         vector<int> v(256,0);
         string s;
-        cin.getline(s);
+        getline(cin,s);
         for(int i=0; i<s.size(); i++){
             v[(int)toupper(s[i])]++;
         }
-        cout<<m[*max_element(v.begin()+65, v.begin()+91)];
+        cout<<"Case "<<q<<": "<<m[*min_element(v.begin()+65, v.begin()+91)]<<"\n";
     }
     return 0;
 }
